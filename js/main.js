3bffaca1b1b16f90934efc54ffdc9be3bf819e06
sweesweett <sweesweet@naver.com>
2022-02-11 14:35:32 +0900

    로그인페이지 중복체크 아이디확인

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 로그인 회원가입 화면 바꾸기
 const signinBtn = document.querySelector('.signup')
 const signupBtn = document.querySelector('.signin')
 const signinEl =document.querySelector('.container .signin')
@@ -21,3 +22,33 @@ signupBtn.addEventListener('click',function(){
   subSignup.classList.remove('show');
   subSignin.classList.add('show');
 })
+
+// 아이디 확인 비밀번호확인
+
+function confirmForm(){
+  const pw =document.getElementById('signup-pw').value
+  const pwCheck=document.getElementById('pw-check').value
+  if (pw.length < 4 || pw.length >8){
+  
+  }
+  if(pw!= '' && pwCheck!='') {
+    if(pw == pwCheck) {
+        document.getElementById('same').innerHTML='비밀번호가 일치합니다.';
+        document.getElementById('same').style.color='black';
+    }
+    else {
+        document.getElementById('same').innerHTML='비밀번호가 일치하지 않습니다.';
+        document.getElementById('same').style.color='red';
+    }
+  }
+  }
+  function validId(){
+    alert('test')
+  }
+  function formSubmit(){
+
+    alert("회원가입 완료!")
+  }
+  function login() {
+    alert("로그인 완료!")
+  }
